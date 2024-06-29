e169a5fb5ff2465e39a637b1bfa004180929a391
Friends_network <chirag.lakum1122@gmail.com>
2024-06-29 18:59:01 +0530

    first commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { SidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const Sidebar = () => {
+    const pathname = usePathname();
+  return (
+     <section className="sticky top-0 left-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+        <div className="flex flex-1 flex-col gap-6">
+            {SidebarLinks.map((link)=>{
+                const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
+
+                return(
+                    <Link 
+                     href={link.route}
+                     key={link.label}
+                     className={cn('flex gap-4 items-center rounded-lg p-4 justify-start ',{
+                        'bg-blue-1' :isActive,
+                     })}
+                    >
+                      <Image 
+                       src={link.imgUrl}
+                       alt={link.label}
+                       height={24}
+                       width={24}
+                      />
+                      <p className='text-lg font-semibold max-lg:hidden'>
+                        {link.label}
+                      </p>
+                    </Link>
+                )
+            })}
+        </div>
+     </section>
+  )
+}
+
+export default Sidebar
---
+++ constants/index.ts
@@ -0,0 +1,35 @@
+export const SidebarLinks = [
+    {
+        label:'Home',
+        route:'/',
+        imgUrl:'/icons/home.svg',
+    },
+    {
+        label:'Upcoming',
+        route:'/upcoming',
+        imgUrl:'/icons/upcoming.svg',
+    },
+    {
+        label:'Previous',
+        route:'/previous',
+        imgUrl:'/icons/previous.svg',
+    },
+    {
+        label:'Recordings',
+        route:'/recordings',
+        imgUrl:'/icons/Video.svg',
+    },
+    {
+        label:'Personal Room',
+        route:'/personal-room',
+        imgUrl:'/icons/add-personal.svg',
+    },
+]
+
+export const  avatarImages = [
+    '/images/avatar-1.jpeg',
+    '/images/avatar-2.jpeg',
+    '/images/avatar-3.jpeg',
+    '/images/avatar-4.jpeg',
+    '/images/avatar-5.jpeg',
+]
